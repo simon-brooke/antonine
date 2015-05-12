f2a48cbe045e6c839e58ca3ed050b56a60af4537
simon <simon@journeyman.cc>
2015-05-12 19:47:43 +0100

    Very minor tidyup; updated the license fields in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject antonine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A calculator which uses Roman numerals."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
